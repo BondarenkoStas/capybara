9db9b35c423d673dfea22af597276b9ffb9c1afd
Stanislav Bondarenko <bondarenko.stanislav.96@gmail.com>
2020-09-16 15:07:40 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-cd android
-echo "bla bla bla" > ffile
-cat ffile
+if [[ "$NEVERCODE_BRANCH" =~ "qa" ]]; then
+    cd android
+    sed -i "" "s/APP_PRODUCTION = true/APP_PRODUCTION = false/g" ffile
+    sed -i "" "s/ANALYTICS_PRODUCTION = true/ANALYTICS_PRODUCTION = false/g" ffile
+    sed -i "" "s/API_ENVIRONMENT = 0/API_ENVIRONMENT = 1/g" ffile
+    cat ffile
+fi
